95417cb8509f4e0725f3952e0d318308076daad0
Jesús García Luque <77391383+Ajgarcia01@users.noreply.github.com>
2022-03-02 12:49:41 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyAnd1nj6O7otlnF-QqHvRgNeJ4NunYDQag",
-    authDomain: "notea-205b6.firebaseapp.com",
-    projectId: "notea-205b6",
-    storageBucket: "notea-205b6.appspot.com",
-    messagingSenderId: "545644817944",
-    appId: "1:545644817944:web:b6f8adfbf1786f2ae6dd6d",
-    measurementId: "G-F961PMC1YD",
-    todoCollection:"todo"
-  }
+ 
 };
